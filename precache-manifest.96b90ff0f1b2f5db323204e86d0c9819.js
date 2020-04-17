e4c08e4ca0ddd8777657730c62d9dc483ad02fe8
lugrao <56491057+lagoru-q@users.noreply.github.com>
2020-04-17 17:36:24 -0300

    Updates

---
+++ precache-manifest.96b90ff0f1b2f5db323204e86d0c9819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d569261ff8e70b9d922e4e0e8dd7b7f6",
+    "url": "/cadex/index.html"
+  },
+  {
+    "revision": "c667df22c6a99932df35",
+    "url": "/cadex/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "77625576bc051a6e9c4f",
+    "url": "/cadex/static/css/main.e046ef0f.chunk.css"
+  },
+  {
+    "revision": "c667df22c6a99932df35",
+    "url": "/cadex/static/js/2.05536e29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cadex/static/js/2.05536e29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77625576bc051a6e9c4f",
+    "url": "/cadex/static/js/main.5192ac0f.chunk.js"
+  },
+  {
+    "revision": "b8213ecc1e111cffd9fe",
+    "url": "/cadex/static/js/runtime-main.3656d1d4.js"
+  }
+]);
